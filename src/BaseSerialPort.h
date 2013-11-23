e4735df424d1ab83168796d0d1e564d425cc5148
oddguid <oddguid@oddguid.com>
2013-11-23 20:08:54 +0100

    Converted public slots to q_invokable for BaseSerialPort

--- src/BaseSerialPort.h
+++ src/BaseSerialPort.h
@@ -20,7 +20,6 @@ public:
   /// Destructor.
   virtual ~BaseSerialPort();
 
-public slots:
   /// Opens the serial port.
   ///
   /// \param[in] deviceName Name of the serial port.
@@ -28,37 +27,37 @@ public slots:
   /// \param[in] timeoutSecs Timeout for serial port in seconds, range is
   /// 0 <= timeoutSecs <= 65535.
   /// \return True if successful, false on error.
-  virtual bool open(const QString &deviceName,
-                    unsigned int baudrate,
-                    unsigned short timeoutSecs) = 0;
+  Q_INVOKABLE virtual bool open(const QString &deviceName,
+                                unsigned int baudrate,
+                                unsigned short timeoutSecs) = 0;
 
   /// Indicates whether serial port is open or not.
   ///
   /// \return True if serial port is open, false otherwise.
-  virtual bool isOpen() = 0;
+  Q_INVOKABLE virtual bool isOpen() = 0;
 
   /// Closes the serial port.
   ///
   /// \return True if successful, false on error.
-  virtual bool close() = 0;
+  Q_INVOKABLE virtual bool close() = 0;
 
   /// Sets the timeout of the serial port.
   ///
   /// \param[in] timeoutSecs Timeout for serial port in seconds, range is
   /// 0 <= timeoutSecs <= 65535.
   /// \return True is successful, false on error.
-  virtual bool setTimeout(unsigned short timeoutSecs) = 0;
+  Q_INVOKABLE virtual bool setTimeout(unsigned short timeoutSecs) = 0;
 
   /// Write the string data to the serial port.
   ///
   /// \param[in] data String data to write.
   /// \return True if successful, false on error.
-  virtual bool write(const QString &data) = 0;
+  Q_INVOKABLE virtual bool write(const QString &data) = 0;
 
   /// Returns the last saved error message.
   ///
   /// \return Error message.
-  QString error() const;
+  Q_INVOKABLE QString error() const;
 };
 
 #endif
