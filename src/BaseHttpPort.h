28e093e58e709bde74ac69dce9e1e2adbb3e3c2e
oddguid <oddguid@oddguid.com>
2013-11-23 20:06:34 +0100

    Converted public slots to q_invokable for BaseHttpPort

--- src/BaseHttpPort.h
+++ src/BaseHttpPort.h
@@ -21,24 +21,23 @@ public:
   /// Destructor.
   virtual ~BaseHttpPort();
 
-public slots:
   /// Gets the data at the given url.
   ///
   /// \param[in] url Url to get data from.
   /// \return True if successful, false if error occurred.
-  virtual bool get(const QUrl &url) = 0;
+  Q_INVOKABLE virtual bool get(const QUrl &url) = 0;
 
   /// Returns the received data. Can be called after get() is successfully
   /// called.
   ///
   /// \return Received data, empty if no data is available.
-  virtual QString data() = 0;
+  Q_INVOKABLE virtual QString data() = 0;
 
   /// Sets the connection timeout in seconds.
   ///
   /// \param[in] timeout Timeout in seconds, range is 1 <= timout <= 65535.
   /// \return True if successful, false if error occurred.
-  virtual bool setTimeout(unsigned short timeout) = 0;
+  Q_INVOKABLE virtual bool setTimeout(unsigned short timeout) = 0;
 
   /// Sets the basic authentication properties. This function must be called
   /// before get().
@@ -46,8 +45,8 @@ public slots:
   /// \param[in] user User name.
   /// \param[in] password Password of user.
   /// \return True if successful, false if error occurred.
-  virtual bool setAuthentication(const QString &user,
-                                 const QString &password) = 0;
+  Q_INVOKABLE virtual bool setAuthentication(const QString &user,
+                                             const QString &password) = 0;
 
   /// Sets the proxy to use. Use an empty host to disable the proxy. This
   /// function must be called before get().
@@ -57,15 +56,15 @@ public slots:
   /// \param[in] host Proxy hostname.
   /// \param[in] port Proxy port number, range is 0 <= port <= 65535.
   /// \return True if successful, false if error occurred.
-  virtual bool setProxy(const QString &user,
-                        const QString &password,
-                        const QString &host,
-                        unsigned short port) = 0;
+  Q_INVOKABLE virtual bool setProxy(const QString &user,
+                                    const QString &password,
+                                    const QString &host,
+                                    unsigned short port) = 0;
 
   /// Returns the last saved error message.
   ///
   /// \return Error message.
-  QString error() const;
+  Q_INVOKABLE QString error() const;
 };
 
 #endif
