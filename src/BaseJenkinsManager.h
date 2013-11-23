fb36c9367d1cd98592a3424c461366378f5265d2
oddguid <oddguid@oddguid.com>
2013-11-23 20:03:17 +0100

    Converted public slots to q_invokable for BaseJenkinsManager

--- src/BaseJenkinsManager.h
+++ src/BaseJenkinsManager.h
@@ -30,27 +30,22 @@ public:
   /// Destructor.
   virtual ~BaseJenkinsManager();
 
-protected:
-  /// Empties the job list.
-  void clearJobs();
-
-public slots:
   /// Retrieves the status of the jobs from the build server at the given URL.
   ///
   /// \param[in] url URL of build server (main status page).
   /// \return True if successful, false if error occurred.
-  virtual bool getStatus(const QString &url) = 0;
+  Q_INVOKABLE virtual bool getStatus(const QString &url) = 0;
 
   /// Returns a list with the names of all jobs found on the build server.
   ///
   /// \return List with job names.
-  virtual QStringList jobNames();
+  Q_INVOKABLE virtual QStringList jobNames();
 
   /// Returns the URL of the given job.
   ///
   /// \param[in] jobName Name of job.
   /// \return URL of job, empty if job not found.
-  virtual QString jobUrl(const QString &jobName);
+  Q_INVOKABLE virtual QString jobUrl(const QString &jobName);
 
   /// Returns the color (status) of the given job. Possible values
   /// are:
@@ -66,29 +61,33 @@ public slots:
   ///
   /// \param[in] jobName Name of job.
   /// \return Color of job, empty if job not found.
-  virtual QString jobColor(const QString &jobName);
+  Q_INVOKABLE virtual QString jobColor(const QString &jobName);
 
   /// Returns a list with all jobs found on the build server.
   ///
   /// \return List with job objects.
-  virtual QObjectList jobs();
+  Q_INVOKABLE virtual QObjectList jobs();
 
   /// Returns the job of the given name.
   ///
   /// \param[in] jobName Name of job.
   /// \return Job object, NULL if job not found.
-  virtual QObject *job(const QString &jobName);
+  Q_INVOKABLE virtual QObject *job(const QString &jobName);
 
   /// Sets the HTTP port to use. This object will not become the parent of the
   /// HTTP port and will therefore not clean it up.
   ///
   /// \param[in] httpPort HTTP port to use.
-  void setHttpPort(BaseHttpPort *port);
+  Q_INVOKABLE void setHttpPort(BaseHttpPort *port);
 
   /// Returns the last saved error message.
   ///
   /// \return Error message.
-  QString error() const;
+  Q_INVOKABLE QString error() const;
+
+protected:
+  /// Empties the job list.
+  void clearJobs();
 };
 
 #endif
