62fde1b5990af571c370c1b382f84e9979635aba
oddguid <oddguid@oddguid.com>
2014-03-10 21:34:32 +0100

    Made function set() in Color invokable in script

--- src/Color.h
+++ src/Color.h
@@ -80,7 +80,7 @@ public:
   /// \param[in] r Red component of color, range 0 <= r <= 255.
   /// \param[in] g Green component of color, range 0 <= g <= 255.
   /// \param[in] b Blue component of color, range 0 <= b <= 255.
-  void set(quint8 r, quint8 g, quint8 b);
+  Q_INVOKABLE void set(quint8 r, quint8 g, quint8 b);
 
   /// Converts the RGB color object to a QScriptValue object. The QScriptValue
   /// object will have the following properties:
